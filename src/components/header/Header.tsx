42b5f6d6e869206ebec71ede14080d5bbe21489b
artempudovkin <apudovkin@i-neti.ru>
2022-09-30 22:50:15 +0500

    feat: Сверстать верхнее меню с моделями

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react';
 import Link from 'next/link';
 import { HeaderSearch } from './HeaderSearch';
 import { HeaderCity } from './HeaderCity';
+import Image from 'next/image';
+import { HeaderDropdown } from './HeaderDropdown';
 
 interface IHeader {
 }
@@ -10,8 +12,8 @@ export const Header: FC<IHeader> = (props) => {
 
   //Renders
   return (
-    <header className='bg-primary flex py-6'>
-      <div className='mx-auto container flex items-center max-w-7xl'>
+    <header className='bg-primary flex pt-6 flex-col'>
+      <div className='mx-auto container flex items-center max-w-7xl pb-6'>
         <div className='flex text-white text-xl max-w-3xl justify-between w-full'>
           <Link href='/'>
             Главная
@@ -36,6 +38,20 @@ export const Header: FC<IHeader> = (props) => {
           <HeaderSearch />
         </div>
         <HeaderCity />
+      </div>
+      <div className='bg-secondary py-3 flex'>
+        <div className='max-w-7xl container mx-auto flex justify-between'>
+          <div className='flex gap-20'>
+            <HeaderDropdown/>
+            <HeaderDropdown/>
+            <HeaderDropdown/>
+            <HeaderDropdown/>
+            <HeaderDropdown/>
+            <HeaderDropdown/>
+          </div>
+
+          <a href='tel:83452123456' className='text-xl text-white'>+7(3452)123-456</a>
+        </div>
       </div>
     </header>
   );
---
+++ src/components/header/HeaderDropdown.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+import Image from 'next/image';
+
+interface IHeaderDropdown {}
+
+export const HeaderDropdown:FC<IHeaderDropdown> = (props) => {
+
+ //Renders
+ return (
+   <div className='dropdown'>
+     <label tabIndex={0} className='text-white text-xl flex gap-2'>iPhone
+       <Image src='/assets/icons/down.svg' width={12} height={12} alt='down arrow' />
+     </label>
+     <ul tabIndex={0} className='dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52'>
+       <li><a>iPhone 14 Pro Max</a></li>
+       <li><a>iPhone 14 Pro</a></li>
+       <li><a>iPhone 13 Pro Max</a></li>
+       <li><a>iPhone 13 Pro</a></li>
+     </ul>
+   </div>
+ );
+};
