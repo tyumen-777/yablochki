74c8fc21c96d5c2ad60e9e39246f60007cc26486
artempudovkin <apudovkin@i-neti.ru>
2022-10-27 01:29:45 +0500

    feat: создать страницу конкретной категории. добавить ссылки в карточках.

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -2,15 +2,17 @@ import React, { FC } from 'react';
 import Image from 'next/image';
 
 interface ICategoryCard {
+  name: string,
+  price: number
 }
 
 export const CategoryCard: FC<ICategoryCard> = (props) => {
-
+  const {name, price} = props
   //Renders
   return (
     <div className='max-w-md flex flex-col bg-white rounded-md shadow pt-4 items-center'>
-      <h2 className='text-2xl'>iPhone</h2>
-      <p>от 49 990 ₽</p>
+      <h2 className='text-2xl'>{name}</h2>
+      <p>от {price} ₽</p>
       <Image src='/assets/images/smalliphone.png' width={210} height='200' alt='iphone' objectFit='contain'/>
     </div>
   );
